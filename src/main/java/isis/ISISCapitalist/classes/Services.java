4ceb1a54b44fe1a937c11189fbd0d17a223d0361
Stéphane <81stephane@gmail.com>
2022-03-08 13:43:26 +0100

    Ajout REST PUT modifyProduct

--- src/main/java/isis/ISISCapitalist/classes/Services.java
+++ src/main/java/isis/ISISCapitalist/classes/Services.java
@@ -29,8 +29,6 @@ public class Services {
     }
 
     public World readWorldFromXml(String pseudo) {
-
-        System.out.println(pseudo.getClass());
         World world = new World();
         InputStream input;
 
@@ -48,6 +46,7 @@ public class Services {
             }
             System.out.println("Final : " + input);
             world = (World) jaxbUnmarshaller.unmarshal(input);
+            input.close();
 
         } catch (Exception ex) {
             System.out.println("Erreur lecture du fichier:" + ex.getMessage());
@@ -60,6 +59,48 @@ public class Services {
 
     public World getWorld(String pseudo) {
         return readWorldFromXml(pseudo);
+    }
+
+    
+    public Boolean updateProduct(String username, ProductType newProduct) {
+        // Aller chercher le monde qui correspond au joueur
+        World world = getWorld(username);
+        ProductType oldProduct = new ProductType();
+        // trouver dans ce monde, le produit équivalent à celui passé
+        // en paramètre
+        for (ProductType product : world.getProducts().getProduct()) {
+            if (product.getId() == newProduct.getId()) {
+                oldProduct = product;
+                break;
+            }
+        };
+        if (oldProduct == null) {
+            return false;
+        }
+        // calculer la variation de quantité. Si elle est positive c'est
+        // que le joueur a acheté une certaine quantité de ce produit
+        // sinon c’est qu’il s’agit d’un lancement de production.
+        int qteChange = newProduct.getQuantite() - oldProduct.getQuantite();
+        if (qteChange > 0) {
+            // soustraire de l'argent du joueur le cout de la quantité achetée
+            double un = newProduct.getCout() * Math.pow(oldProduct.getCroissance(), oldProduct.getQuantite());
+            double numerator = 1 - Math.pow(oldProduct.getCroissance(), qteChange);
+            double denominator = 1 - oldProduct.getCroissance();
+            double quantityCost = (un * numerator) / denominator;
+            
+            double newMoney = world.getMoney() - quantityCost;
+            world.setMoney(newMoney);
+            // et mettre à jour la quantité de product
+            oldProduct.setQuantite(newProduct.getQuantite());
+        } else {
+            // initialiser product.timeleft à product.vitesse
+            oldProduct.setTimeleft(oldProduct.getVitesse());
+            // pour lancer la production
+            
+        }
+        // sauvegarder les changements du monde
+        saveWorldToXml(world, username);
+        return true;
     }
 
 }
--- src/main/java/isis/ISISCapitalist/classes/WebService.java
+++ src/main/java/isis/ISISCapitalist/classes/WebService.java
@@ -1,5 +1,7 @@
 package isis.ISISCapitalist.classes;
 
+import java.lang.reflect.Field;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,7 +19,36 @@ public class WebService {
     // Affichage du monde customisé
     @GetMapping(value = "world", produces = {"application/xml", "application/json"})
     public ResponseEntity<World> getWorld(@RequestHeader(value = "X-User", required = false) String username) {
+        // On récupère le monde à partir du pseudo
         World world = services.getWorld(username);
+        
+        // On retourne le monde existant ou nouvellement crée
         return ResponseEntity.ok(world);
     }
+    
+    // Modification d'un produit
+    @PutMapping(value = "product", consumes = {"application/xml", "application/json"})
+    public ResponseEntity<ProductType> modifyProduct(@RequestBody ProductType newProduct, @RequestHeader(value = "X-User", required = false) String username) {
+        // On récupère le monde à partir du pseudo
+        World world = services.getWorld(username);
+        
+        // On actualise le produit
+        if (services.updateProduct(username, newProduct)) {
+            return ResponseEntity.ok(newProduct);
+        }
+        else {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+    }
+    
+    
+    // Modification d'un produit
+    @PutMapping(value = "product", consumes = {"application/xml", "application/json"})
+    public ResponseEntity<PallierType> modifyManager(@RequestBody PallierType newManager, @RequestHeader(value = "X-User", required = false) String username) {
+        // On récupère le monde à partir du pseudo
+        World world = services.getWorld(username);
+        
+        // On renvoie le manager
+        return ResponseEntity.ok(newManager);
+    } 
 }
